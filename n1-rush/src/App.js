f5dff9ff2aa6d3cf4fa0859649d7b8c20dd90bc4
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-09-26 02:11:48 -0400

    Add HomePage

--- n1-rush/src/App.js
+++ n1-rush/src/App.js
@@ -1,7 +1,7 @@
-import Header from './components/Header';
+import HomePage from './components/pages/HomePage';
 
 function App() {
-  return <Header />;
+  return <HomePage />;
 }
 
 export default App;
---
+++ n1-rush/src/components/pages/HomePage/index.jsx
@@ -0,0 +1,14 @@
+import Header from '../../Header';
+import Hero from '../../Hero';
+
+const HomePage = () => (
+  <>
+    <Header />
+
+    <main>
+      <Hero />
+    </main>
+  </>
+);
+
+export default HomePage;
