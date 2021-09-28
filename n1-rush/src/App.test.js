f177fc6d893a1df61339965394d5f26ead1a7dd2
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-09-28 13:35:31 -0400

    Add Context in App test

--- n1-rush/src/App.test.js
+++ n1-rush/src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
+
 import App from './App';
+import ContextProvider from './providers/ContextProvider';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(
+    <ContextProvider>
+      <App />
+    </ContextProvider>,
+  );
   const linkElement = screen.getByText(/N1 Rush/i);
   expect(linkElement).toBeInTheDocument();
 });
